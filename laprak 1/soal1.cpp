d7d0e7f3723af610750251761210f296d878bde0
mahardhikaputra469-dotcom <mahardhikaputra469@gmail.com>
2026-09-24 18:55:13 +0700

    Add files via upload

---
+++ laprak 1/soal1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+
+    cout << "Masukkan bilangan pertama: ";
+    cin >> a;
+
+    cout << "Masukkan bilangan kedua: ";
+    cin >> b;
+
+    cout << "Penjumlahan = " << a + b << endl;
+    cout << "Pengurangan = " << a - b << endl;
+    cout << "Perkalian   = " << a * b << endl;
+    cout << "Pembagian   = " << a / b << endl;
+
+    return 0;
+}
